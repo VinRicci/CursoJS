d44107bcdd397227f51e920436faeca0429f93a3
Vinicio <vinicioricci@gmail.com>
2022-10-08 21:08:08 -0600

    Tareas

---
+++ segunda/segunda.js
@@ -0,0 +1,14 @@
+const nombre = "Vinicio"
+const edad = "23"
+const dev = true
+const nac = new Date(1999, 10, 11)
+const libro = {
+    nombre: "El replandor",
+    autor: "Stephen King",
+    fecha: 1980,
+    url: "https://es.wikipedia.org/wiki/El_resplandor_(novela)"
+}
+
+const lista = [nombre, edad, dev, nac, libro]
+
+console.log(lista)
